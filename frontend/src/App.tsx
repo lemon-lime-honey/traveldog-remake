d8f19bfec5473e0ce044a1cd9ab9f382af5c7ecb
lemon-lime-honey <bs0308@g.skku.edu>
2023-12-27 17:16:46 +0900

    feat: add appbar
    - 상단부 바 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,22 @@
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container maxWidth="xl">
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">TravelDog</Typography>
+          </Toolbar>
+        </AppBar>
+      </Container>
+    </>
+  );
 }
 
 export default App;
