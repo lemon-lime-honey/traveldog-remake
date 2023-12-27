2f5329f9d7cf297f28387f4a8ffa810a92c6f4c8
lemon-lime-honey <bs0308@g.skku.edu>
2023-12-27 17:16:25 +0900

    feat: add custom theme
    - 커스텀 테마 설정

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import { theme } from './assets/Styles/theme.ts';
+import { ThemeProvider } from '@emotion/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 );
